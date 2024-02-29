0a7b34816035d0e810f300679c05a378822e2237
satishkumargovinda <satishkumar.govinda@gmail.com>
2024-02-29 21:15:56 +0530

    "Updated Spurce Code"

--- src/main/python/helloworld.py
+++ src/main/python/helloworld.py
@@ -1,4 +1,23 @@
+"""
+This is a simple module to demonstrate a hello world function in Python.
+
+"""
+
 import sys
 
 def helloworld(out):
-    out.write("Hello world of Python\n")
+    """
+    Print a hello world message
+    
+
+    Args:
+        out: Output stream to write the message to.
+
+    """
+
+    out.write("Hello world Python\n")
+
+# Call the function with a file object.
+
+helloworld(sys.stdout)
+    
